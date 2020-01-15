71dd939fa92decd63da57c5ec95fdf97bf2d740c
vzelinsky <vzelinsky@albany.edu>
2020-01-15 16:10:31 -0500

    Configured postgres db

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,13 @@ ENV PYTHONUNBUFFERED 1
 
 # copy local requirements.txt to the container
 COPY ./requirements.txt /requirements.txt
+RUN apk add --update --no-cache postgresql-client
+# for minimal footprint
+RUN apk add --update --no-cache --virtual .tmp-build-deps \
+        gcc libc-dev linux-headers postgresql-dev
 # install requirements using pip
 RUN pip install -r requirements.txt
+RUN apk del .tmp-build-deps
 
 # make a dir called app in container
 RUN mkdir /app
---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,25 @@
+from unittest.mock import patch
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import TestCase
+
+
+class CommandTests(TestCase):
+
+    def test_wait_for_db_ready(self):
+        """Test waiting for db when db is available"""
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            # instead of actually preforming this operation mock it
+            gi.return_value = True
+            call_command('wait_for_db')
+            self.assertEqual(gi.call_count, 1)
+
+    @patch('time.sleep', return_value=True)  # just speeds up test
+    def test_wait_for_db(self, ts):
+        """Test waiting for db"""
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            # raise operational error 5 times
+            gi.side_effect = [OperationalError] * 5 + [True]
+            call_command('wait_for_db')
+            self.assertEqual(gi.call_count, 6)
